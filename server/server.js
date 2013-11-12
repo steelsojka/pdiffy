c2905de831fac645c456d28cc22b48760c5c13bb
steelsojka <steelsojka@gmail.com>
2013-11-12 08:22:51 -0600

    add(server): add envionment

--- server/server.js
+++ server/server.js
@@ -15,16 +15,17 @@ app.configure("development", function() {
   }));
 
   app.use(express.static(path.join(__dirname, './../.tmp')));
+  app.use(express.static(path.join(__dirname, './../app')));
 });
 
 app.configure("production", function() {
   app.use(express.errorHandler());
+  app.use(express.static(path.join(__dirname, './../dist/app')));
 });
 
-app.use(express.static(path.join(__dirname, './../app')));
 
 exports.io = io.listen(http.createServer(app).listen(app.get("port"), function() {
-  console.log("Application running at http://localhost:" + app.get("port"));
+  console.log("Application running at http://localhost:" + app.get("port") + " on " + process.env.NODE_ENV);
 }));
 
 require("./sockets");
