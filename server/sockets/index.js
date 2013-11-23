7092ba3bafd539b7f7ac8b002aa986828c82e740
steelsojka <steelsojka@gmail.com>
2013-11-23 16:41:43 -0600

    fix(config): fix path to config

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -5,6 +5,7 @@ var io = require( './../server' ).io;
 var webshot = require("webshot");
 var fs = require('fs');
 var _ = require("lodash");
+var path = require("path");
 
 // Connection route - bootstraps the other socket routes
 io.sockets.on('connection', function( socket ) {
@@ -29,7 +30,7 @@ io.sockets.on('connection', function( socket ) {
   });
 
   socket.on("send:getConfig", function() {
-    var config = fs.readFile("server/config-defaults.json", "utf8", function(err, data) {
+    var config = fs.readFile(path.join(__dirname, "../config-defaults.json"), "utf8", function(err, data) {
       if (err) throw err;
       socket.emit("data:config", data);
     });
